d75bddf06f1f3b291f4aa33b2fc565de5cd0746d
Marco Zangari <mjzangari@gmail.com>
2017-10-21 21:51:14 -0700

    test added move all vowels

---
+++ src/test_filter_even_length_words.py
@@ -0,0 +1,10 @@
+"""Tests the filter even length words kata."""
+import pytest
+
+FIND_PARAMS = [(["Hello", "World"]),[], (["One", "Two", "Three", "Four"],["Four"]), (["Bob", "Rhino", "Paul"], ["Paul"])]
+
+@pytest.mark.parametrize('n, result', FIND_PARAMS)
+def test_find_it(n, result):
+	"""Test for find_it function."""
+	from filter_even_length_words import filter_even_length_words
+	assert filter_even_length_words(n) == result
