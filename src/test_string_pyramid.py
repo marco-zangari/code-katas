97dc7891a353d430dfc4d7c1b358ee59152f44e2
Marco Zangari <mjzangari@gmail.com>
2018-01-05 09:50:07 -0800

    added two tests. one for single char input and another for multiple char input.

--- src/test_string_pyramid.py
+++ src/test_string_pyramid.py
@@ -6,7 +6,13 @@ import pytest
 from string_pyramid import watch_pyramid_from_the_side
 
 
-def test_watch_pyramid_from_the_side():
+def test_watch_pyramid_from_the_side_one_char():
+    """Test from the side."""
+    output = watch_pyramid_from_the_side('a')
+    assert output == 'a\n'
+
+
+def test_watch_pyramid_from_the_side_multiple_char():
     """Test from the side."""
     output = watch_pyramid_from_the_side('abc')
-    assert output = '  c  \n bbb \naaaaa'
+    assert output == '  c  \n bbb \naaaaa\n'
