89f88102e07869958a2be1ca79900e636733495a
Marco Zangari <mjzangari@gmail.com>
2018-01-05 17:42:55 -0800

    added tests and more functions

--- src/string_pyramid.py
+++ src/string_pyramid.py
@@ -22,5 +22,44 @@ def watch_pyramid_from_the_side(characters):
     return "".join(newer_container)
 
 
-def watch_pyramid_from_above(alist):
+def watch_pyramid_from_above(characters):
     """String pyramid implementation view from above."""
+    str_len = len(characters)
+    count = str_len + (str_len - 1)
+    container = []
+    max_count = count
+    min_count = 0
+    if count == 1:
+        print(characters)
+    for num in range(count):
+        char = characters[0] * count
+        container.append(list(char))
+    max_count -= 1
+    min_count += 1
+    count -= 2
+    while count > 0:
+        for ch in characters[1:]:
+            for alist in container[min_count: max_count]:
+                alist[min_count: max_count] = ch * count
+            max_count -= 1
+            min_count += 1
+            count -= 2
+    return container
+
+
+def count_visible_characters_of_the_pyramid(characters):
+    """String count."""
+    str_len = len(characters)
+    count = str_len + (str_len - 1)
+    return count ** 2
+
+
+def count_all_characters_of_the_pyramid(characters):
+    """Count all characters."""
+    character_count = 0
+    size = len(characters)
+    while size > 0:
+        size_more = (size + (size - 1)) ** 2
+        character_count += size_more
+        size -= 1
+    return character_count
