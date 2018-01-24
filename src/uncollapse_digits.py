d1f8cfef367b242f860feadff28acbe820831d8f
Marco Zangari <mjzangari@gmail.com>
2018-01-23 18:04:24 -0800

    added new kata uncollapse digits

---
+++ src/uncollapse_digits.py
@@ -0,0 +1,10 @@
+"""Uncollapse Digits, codewars kata, level 6."""
+
+
+def uncollapse(s):
+    """Return uncollapse digit string.
+
+    input: string
+    output: string with spaces between numbers
+    ex: fivethreefivesixthreenineonesevenoneeight -> five three five six three nine one seven one eight
+    """
