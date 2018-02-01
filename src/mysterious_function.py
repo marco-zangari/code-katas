686ad466f8d27275b34bb676be346f14e9e81b79
Marco Zangari <mjzangari@gmail.com>
2018-01-31 16:45:00 -0800

    kata mysterious function

--- src/mysterious_function.py
+++ src/mysterious_function.py
@@ -12,10 +12,13 @@ def mysterious_func(nums):
         getNum(89282350306) #-> returns 8
         getNum(3479283469) #-> returns 5
     """
-    holes = [0, 6, 8, 9]
+    holes = ['0', '6', '8', '9']
     hole_count = 0
     num_st = str(nums)
     for num in num_st:
         if num in holes:
-            hole_count += 1
+            if num == '8':
+                hole_count += 2
+            else:
+                hole_count += 1
     return hole_count
