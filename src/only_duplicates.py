07ba0e5f0568b7c5dcc93c134b5b6bfd284e45ec
Marco Zangari <mjzangari@gmail.com>
2018-02-05 22:11:55 -0800

    adding new kata, only_duplicates

--- src/only_duplicates.py
+++ src/only_duplicates.py
@@ -3,3 +3,15 @@
 
 def remove_unique(s):
     """Remove unique elements from a string."""
+    res = []
+    dict_obj = histo(s)
+    for ch in s:
+        if dict_obj[ch] == 1:
+            pass
+        else:
+            res.append(ch)
+    return ''.join(res)
+
+if __name__ == '__main__':
+    s = 'abccdefee'
+    remove_unique(s)
