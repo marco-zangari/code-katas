933979c5ab6a00e99204866134b90515b4273d5b
Marco Zangari <mjzangari@gmail.com>
2018-02-11 14:33:25 -0800

    linked_list push

--- src/linked_list_push.py
+++ src/linked_list_push.py
@@ -2,16 +2,21 @@
 
 
 class Node(object):
+    """Build class for node object."""
+
     def __init__(self, data):
+        """Init method."""
         self.data = data
         self.next = None
 
 def push(head, data):
+    """Push method on linked list."""
   new_node = Node(data)
   new_node.next = head
   return new_node
 
 def build_one_two_three():
+    """Build out the linked list with three nodes."""
     chained = None
     chained = push(chained, 3)
     chained = push(chained, 2)
